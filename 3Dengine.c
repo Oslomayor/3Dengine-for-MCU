6b966aae7dd87b33b9331a89a7d1274d4c0378df
Denis <34310801+Oslomayor@users.noreply.github.com>
2018-09-11 20:34:57 +0800

    Update 3Dengine.c

--- 3Dengine.c
+++ 3Dengine.c
@@ -1,17 +1,21 @@
 /* 3Dengine by Oslomayor,2018 Aug 24th,15:20 */
 
 #include <stdio.h>
-#include <math,h>
+#include <math.h>
 
 /* size of 3D Buff */
 #define SIZE 255
 
 /* 3D data Buff */
-char _3D_Buff[SIZE][SIZE][SIZE]
+char $3D_Buff[SIZE][SIZE][SIZE]
 /* display data Buff */
 char Display_Buff[SIZE][SIZE][SIZE]
 
-/* Initialize the 3D Buffer */
+/** 
+  * @brief  Initialize the 3D Data Buffer
+  * @note   Be careful about the buffer SIZE
+  * @param  None 
+  */
 void init_3D_Buff()
 {
     unsigned char x,y,z;
@@ -22,15 +26,26 @@ void init_3D_Buff()
                 if(128<=z&&z<=137)    
                 {
                     if((x-127)*(x-127)+(y-127)*(y-127)==900)
-                        _3D_Buff[x][y][z]=1;
+                        $3D_Buff[x][y][z]=1;
                     else
-                        _3D_Buff[x][y][z]=0;
+                        $3D_Buff[x][y][z]=0;
                 }
             }
     return;
 }
 
-void turn(char xyz, char theta)
+/** 
+  * @brief  Rotate the 3D Module in the Data Buffer
+  * @note   
+  * @param  xyz: where the 3D Mudule can be rotated via certain coordinate axis
+  *           This parameter can be one of the following values:
+  *            @arg 'x': rotate via x axis
+  *            @arg 'y': rotate via y axis
+  *            @arg 'z': rotate via z axis
+  * @param  theta: where the 3D Mudule rotation angle can be set
+  *           This parameter can be set from [0,360]
+  */
+void rotate(char xyz, char theta)
 {
     unsigned char x,y,z;
     for(x=0;x<=SIZE;x++)
@@ -58,26 +73,62 @@ void turn(char xyz, char theta)
                         z_temp = z*cos(theta)+y*sin(theta);
                         break;
                     }
-                    for(x=0;x<=SIZE;x++)
+                }
+                for(x=0;x<=SIZE;x++)
                         for(y=0;y<=SIZE;y++) 
                             for(z=0;z<=SIZE;z++)
-                            {
-                                _3D_Buff[x][y][z]=_3d_Buff_Temp[x][y][z];
-                            }
-                }
+                                $3D_Buff[x][y][z]=_3d_Buff_Temp[x][y][z];
             }
     return;
 }
 
+/**
+  * @breaf  Project the 3D Mudule to a plane to display
+  * @note   This function can help you transform 3D data to 2D data
+  *         in selected direction
+  * @param  xyz: where the project direction can be selected
+  *           This parameter can be one of the following values:
+  *            @arg 'x': project to yz plane
+  *            @arg 'y': project to zx plane
+  *            @arg 'z': project to xy plane
+  */
 void project(char xyz)
 {
     /* screen x,y */
-    unsigned char xx,yy;
+    unsigned char x_screen,y_screen;
     switch(xyz)
     {
+        case 'x': 
+            for(y=0;y<=SIZE;y++)
+                for(z=0;z<=SIZE;z++)
+                    for(x=0;x<=SIZE;x++)
+                    {
+                        if(0 != $3D_Buff[x][y][z])
+                            Display_Buff[y][z] = $3D_Buff[x][y][z];
+                        else
+                            Display_Buff[y][z] = 0;
+                    }break;
+        case 'y': 
+            for(z=0;z<=SIZE;z++)
+                for(x=0;x<=SIZE;x++)
+                    for(y=0;y<=SIZE;y++)
+                    {
+                        if(0 != $3D_Buff[x][y][z])
+                            Display_Buff[z][x] = $3D_Buff[x][y][z];
+                        else
+                            Display_Buff[z][x] = 0;
+                    }break;
 
+        case 'z': 
+            for(x=0;x<=SIZE;x++)
+                for(y=0;y<=SIZE;y++)
+                    for(z=0;z<=SIZE;z++)
+                    {
+                        if(0 != $3D_Buff[x][y][z])
+                            Display_Buff[x][y] = $3D_Buff[x][y][z];
+                        else
+                            Display_Buff[x][y] = 0;
+                    }break;
     }
 }
-
-
 
